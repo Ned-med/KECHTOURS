50a03e862900ed519e71f7b79bd3c97955277a19
MOHAMED MOUIGUINA <mohamedmouiguina7@gmail.COM>
2020-01-30 11:13:37 +0100

    About Section, Cool Photo compisition And Secondary heading animation

--- KechTours/gulpfile.js
+++ KechTours/gulpfile.js
@@ -8,9 +8,24 @@ function style() {
     // 1. where is my scss file
     return gulp.src('./sass/**/*.scss')
     // 2. pass the file through sass compiler
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
     // 3. where do i save the compiled CSS ?
     .pipe(gulp.dest('./css'))
+    // 4. strem changes to all browsers
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./sass/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+
 }
 
 exports.style = style;
+exports.watch = watch;
